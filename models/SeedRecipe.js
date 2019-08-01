fd7644fc61ab0f906e620346d7937cb2fe2299c2
Michael Roth <michael.franklin.roth@gmail.com>
2019-08-01 15:49:14 -0400

    route to grab recipeSummary is either grabbing a recipe from SeedRecipe collection, or is calling Spoonacular API, then creating a SeedRecipe in the route, then passing the newly created SeedRecipe to React App

---
+++ models/SeedRecipe.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const seedRecipeSchema = new Schema({
+  spoonID: String,
+  title: String,
+  ingredients: Array,
+  instructions: Array,
+});
+
+const SeedRecipe = mongoose.model('seedRecipe', seedRecipeSchema);
+
+module.exports = SeedRecipe;
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const SeedRecipe = require('../models/SeedRecipe')
+
 function generateFoodApi(queryString) {
   return (
     axios.create({
@@ -32,17 +34,61 @@ router.get('/search/:theQuery', (req, res, next) => {
     })
 })
 
+// Checks to see if Recipe was already created
+// If already in DB, then pass in that recipe
+// If not, then call to Spoonacular, get the info, and create the recipe on my end
 router.get('/:recipeID/information', (req, res, next) => {
 
-  const theSearch = `/recipes/${req.params.recipeID}/information`
+  // check if recipeID exists in my seedRecipe
+  SeedRecipe.findOne({ spoonID: req.params.recipeID })
+    .then((theResult) => {
+      if (theResult) {
+        // if it does, then send to React Side
+        res.json(theResult)
+      } else {
+        // if not, make call to API, get info, and create the seed here (on the server)
+        const theSearch = `/recipes/${req.params.recipeID}/information`
 
-  generateFoodApi(theSearch).get()
-    .then((response) => {
-      res.json(response.data) // should return summary of a single recipe
+        generateFoodApi(theSearch).get()
+          .then((spoonData) => {
+
+            let grabbedIngredients = spoonData.data.extendedIngredients.map((eachI) => {
+              return ({
+                spoonID: eachI.id,
+                name: eachI.name,
+                usAmount: eachI.measures.us.amount,
+                usUnit: eachI.measures.us.unitLong,
+                metricAmount: eachI.measures.metric.amount,
+                metricUnit: eachI.measures.metric.unitLong,
+              })
+            })
+
+            SeedRecipe.create({
+              spoonID: spoonData.data.id,
+              title: spoonData.data.title,
+              ingredients: grabbedIngredients,
+              instructions: spoonData.data.analyzedInstructions[0].steps,
+            })
+              .then((freshlyCreatedRecipe) => {
+                res.json(freshlyCreatedRecipe)
+              })
+              .catch((err) => {
+                res.json(err)
+              })
+            // res.json(response.data) // should return summary of a single recipe
+          })
+          .catch((err) => {
+            res.json(err)
+          })
+      }
     })
     .catch((err) => {
       res.json(err)
     })
+
+  // once created, then pass my seedRecipe that I just created
+
+
 })
 
 
